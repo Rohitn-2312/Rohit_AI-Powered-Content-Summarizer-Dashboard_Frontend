b6d4016133044ea67661921ac177d9787d8d391f
Rohit Nayak <rohitn822@gmail.com>
2024-07-02 02:32:02 +0530

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,6 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
-        <div className="flex">
-        </div>
-      </nav>
-
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
         <span className="orange_gradient">OpenAI GPT-4</span>
